3fa4a5c67a2fa1e5406a2fa939f75bad804501f9
lronelove <657828543@qq.com>
2020-01-09 11:40:28 +0800

    支持多页面

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,37 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.config.js');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const host = '192.168.5.51'
+const merge = require('webpack-merge')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const common = require('./webpack.config.js')
+
+const host = '192.168.21.202'
+
+// 多页面配置(可以在这里配置多个页面)
+const multiplePagesConfig = [
+	{
+		filename: 'index.html',
+		template: './index.html'
+	},
+	{
+		filename: 'login.html',
+		template: './login.html'
+	}
+]
+
+// 根据多页面配置生成HtmlWebpackPlugin配置
+function generatePluginConfig () {
+	return multiplePagesConfig.map(item => {
+		return new HtmlWebpackPlugin({
+			// 生成出来的html文件名
+			filename: item.filename,
+			// 每个html的模版，这里多个页面使用同一个模版
+			template: item.template,
+			// 自动将引用插入html
+			inject: true
+		})
+	})
+}
 
 module.exports = merge(common, {
   mode: 'development',
@@ -30,6 +59,7 @@ module.exports = merge(common, {
       { from: 'static/css/*' },
       { from: 'static/font/*' },
       { from: 'static/imgs/*' }
-    ])
+    ]),
+	  ...generatePluginConfig() // 根据多页面配置生成HtmlWebpackPlugin配置
   ]
 });
